43efb819cdcd277adbefcb3f02e1445797da4c4c
jschuhmann47 <jschuhmann@frba.utn.edu.ar>
2022-09-23 12:55:34 -0300

    add chapter-3, exercises 1 and 2

--- chapter-3/src/main.rs
+++ chapter-3/src/main.rs
@@ -4,7 +4,7 @@ Generate the nth Fibonacci number.
 Print the lyrics to the Christmas carol “The Twelve Days of Christmas,” taking advantage of the repetition in the song.
 */
 
-use std::default;
+//use std::default;
 
 
 fn main() {
